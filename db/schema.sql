dde3995b89df9e8c11a6c6ffeeaae49579475dbc
BlaiseAK <blaise.a.kress@gmail.com>
2018-07-16 10:41:50 -0400

    Created the schema.sql and put the server.js files.

---
+++ db/schema.sql
@@ -0,0 +1,14 @@
+-- Create a new database and use it in sql
+CREATE DATABASE burgers_db;
+USE burgers_db;
+
+-- Create a new table
+CREATE TABLE burgers (
+    -- Every new burger gets a unique record number
+    id INT NOT NULL AUTO_INCREMENT,
+    -- Every burger gets 255 characters to be stored for their burger
+    burger_name VARCHAR(255) NOT NULL,
+    -- Boolean that is set to default false so it is easier to snap to which area we'd like to attach it to upon load
+    devoured BOOLEAN DEFAULT false,
+    PRIMARY KEY (id)
+)
---
+++ server.js
@@ -0,0 +1,33 @@
+// Required npm files and port set up
+var express = require("express");
+
+var bodyParser = require("body-parser");
+
+var app = express();
+
+var PORT = process.env.PORT || 6969;
+
+// Serve static content for the app from the public directory in the application directory
+app.use(express.static("public"));
+
+// Parse application/urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+
+// Parse application/json
+app.use(bodyParser.json());
+
+// Set Handlebars
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main"}));
+app.set("view engine", "handlebars");
+
+// Import routes and give the server access to them
+var routes = require("./controllers/burgers-controller.js");
+
+app.use(routes);
+
+// Start server to begin listening to client requests
+app.listen(PORT, function() {
+    console.log("Server listening on: http//localhost:" + PORT);
+});
