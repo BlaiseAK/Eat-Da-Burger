c9ebb5415218626695b21471a9d093f04ad072a6
BlaiseAK <blaise.a.kress@gmail.com>
2018-07-17 11:43:50 -0400

    Made some changes to get the burger names to appear with the seeds.sql file. Confirmed DB and GET are working correctly with Handlebars files

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ("Royale with Cheese");
+INSERT INTO burgers (burger_name) VALUES ("Double Cheese Burger with Lettuce, Tomato, Pickle, Ketchup, and Mayo");
+INSERT INTO burgers (burger_name) VALUES ("Ham burger.....nothing on it")
